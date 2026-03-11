1ec55911d7412efb022ea5a455a7c61a6fa0da4a
Leandro Costa <dev.leandro.91@gmail.com>
2026-03-11 12:10:39 -0300

    new pins for lights

--- car/config.h
+++ car/config.h
@@ -1,10 +1,10 @@
 #pragma once
 
 // LEDs
-#define PIN_LED_HEADLIGHT  8
-#define PIN_LED_TAIL       9
-#define PIN_LED_LEFT       10
-#define PIN_LED_RIGHT      20
+#define PIN_LED_HEADLIGHT    8
+#define PIN_LED_TAIL         10
+#define PIN_LED_LEFT         9
+#define PIN_LED_RIGHT        20
 
 // Pisca
 #define BLINK_INTERVAL_MS    500
@@ -14,4 +14,4 @@
 
 // FAILSAFE
 // #define FAILSAFE_TIMEOUT_MS  300   // 300 ms sem comando
-#define FAILSAFE_TIMEOUT_MS 3000
+#define FAILSAFE_TIMEOUT_MS  3000
